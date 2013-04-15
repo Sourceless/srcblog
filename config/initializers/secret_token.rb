08648009c68e77a50bd73c728c697f1d874c1213
Laurence Joseph Smith <laurence@sourceless.org>
2013-04-15 16:49:18 +0100

    Generate rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Srcblog::Application.config.secret_token = '6603106061f3d92a3633aa59990b97828788bc32c32b0134b4c7266820026e89b701934785787c14da4902d74505ede6bd906e07052d5fd450aa6c5f94637f18'
