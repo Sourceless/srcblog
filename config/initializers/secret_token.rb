24764376b243543d7931db42fa260470056681d2
Laurence Joseph Smith <laurence@sourceless.org>
2013-04-15 17:16:31 +0100

    Update to use ENV for SECRET_TOKEN

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Srcblog::Application.config.secret_token = '6603106061f3d92a3633aa59990b97828788bc32c32b0134b4c7266820026e89b701934785787c14da4902d74505ede6bd906e07052d5fd450aa6c5f94637f18'
+Srcblog::Application.config.secret_token = ENV['SECRET_TOKEN'] 
